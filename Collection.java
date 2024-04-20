470817671e021158d51df4ef812004aadf4b5b66
BulatNuriev <143898862+BulatNuriev@users.noreply.github.com>
2024-04-20 11:10:33 +0300

    iterator and comporator in array and linked

---
+++ Collection.java
@@ -0,0 +1,15 @@
+package iterator;
+import java.util.Iterator;
+
+
+public interface Collection<E> {
+    void add(E element);
+    void add(int index, E element);
+    boolean removeElement(E element);
+    void clear();
+    E findElement(E element);
+    int findIndex(E element);
+    boolean removeIndex(int index);
+    int size();
+    Iterator<E> iterator();
+}
